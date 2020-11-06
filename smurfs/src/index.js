e19ec13e97f863348c44ec66f5af5e6219e29b90
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-11-06 08:11:16 -0800

    initial

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
+import React from "react"
+import ReactDOM from "react-dom"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import "./index.css"
+import App from "./components/App"
+import { reducer } from './reducers'
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root"))
